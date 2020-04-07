c01632c17f5d763c6b6454843d320861eda6e162
jeonhyun9 <jhg0675@naver.com>
2020-04-07 10:32:45 +0900

    맵툴 프로젝트 추가
    초기상태 프로젝트 추가

---
+++ mapTool_v0.0/maptool.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "maptool.h"
+
+HRESULT maptool::init()
+{
+	return S_OK;
+}
+
+void maptool::release()
+{
+}
+
+void maptool::update()
+{
+}
+
+void maptool::render()
+{
+}
