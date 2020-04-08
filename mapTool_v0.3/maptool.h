a6541ade1633b217731174cc2d705892d78c37bd
전현구 <jhg0675@naver.com>
2020-04-08 17:30:58 +0900

    맵툴 v0.3
    이준 드래그,미니맵 + 연우 이레이저,리셋  + 동주 세이브,로드

---
+++ mapTool_v0.3/maptool.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "gameNode.h"
+#include "tileNode.h"
+class maptool : public gameNode
+{
+private:
+	tagTile sTile[TILEX * TILEY];
+	tagTile sMiniTile[TILEX * TILEY];
+	tagSampleTile sSampleTile[SAMPLETILEX * SAMPLETILEY];
+	tagCurrentTile sCurrentTile;
+	tagToolBox sToolBox;
+	tagToolBtn sToolBtn;
+
+	image* imgTile;
+
+	CTRL ctrlSelect;
+
+	bool isDrag;
+	RECT dragRc;
+	float saveX, saveY;
+
+	//윈사이즈 렉트
+	RECT rcWins;
+	//배경화면 렉트
+	RECT rcBg;
+	//미니맵 렉트
+	RECT rcMini;
+	RECT rcMcamera;
+
+	//버튼클릭이미지 렌더용			//불변수 추가
+	bool isSave;
+	bool isLoad;
+	bool isObject;
+	bool isTerrain;
+	bool isDragBox;
+	bool isErase;
+	bool isReset;
+	
+
+public:
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	//타일 셋업
+	void tileSetup();
+	void setMap();
+
+	//세이브 로드
+	void save();
+	void load();
+
+
+	//툴박스 업데이트
+	void toolBoxUpdate();
+
+	//지형 세터
+	TERRAIN terrainSelect(int frameX, int frameY);
+	OBJECT objectSelect(int frameX, int frameY);
+
+	maptool() {}
+	~maptool() {}
+};
+
